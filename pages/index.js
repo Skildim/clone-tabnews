87fb4c42c7213c16525eb6c05ae9a3488e186480
Skildim <cassol.heitor@gmail.com>
2026-03-07 01:57:45 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Gabriela vitoria klein cassol voce é o amor da minha vida </h1>
+    return <h1>Gabriela vitoria klein cassol voce é o amor da minha vida, te amo </h1>
 }
 
 export default Home;
