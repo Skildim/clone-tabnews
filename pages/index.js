fbd0af1280215ce5ee40f56a4dad3a4d08a196eb
Skildim <cassol.heitor@gmail.com>
2026-03-13 01:33:17 +0000

    adiciona lint

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
-function Home(){
-    return <h1>Gabriela vitoria klein cassol voce é o amor da minha vida, te amo </h1>
+function Home() {
+  return (
+    <h1>Gabriela vitoria klein cassol voce é o amor da minha vida, te amo </h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
 }
 
 export default Home;
